47e1317117c8be7aaf6d7d42e88ddbda70ae9f5f
Joel Wetzell <jwetzell@yahoo.com>
2016-04-18 13:33:56 -0500

    Fix attach probe option in right-click menu

--- src/jls/elem/JumpEnd.java
+++ src/jls/elem/JumpEnd.java
@@ -402,7 +402,7 @@ public class JumpEnd extends LogicElement {
 			window.add(heading);
 			Object objects[] = circuit.getJumpStartNames().toArray();
 			String strings[] = Arrays.copyOf(objects, objects.length,String[].class); //new way of converting Object[] to String[]
-			starts = new JList<String>(strings);// Old way of conversion (String[])circuit.getJumpStartNames().toArray());
+			starts = new JList<String>(strings);//(String[])circuit.getJumpStartNames().toArray());
 			starts.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			starts.setVisibleRowCount(Math.min(circuit.getJumpStartNames().size(),10));
 			JScrollPane pane = new JScrollPane(starts);
